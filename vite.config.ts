7de582b1b816d2c908fd71d9c7554398f10a462e
Pedro Silva <pedro.o.silva975@gmail.com>
2026-04-20 12:34:40 -0300

    chore: ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     proxy: {
-      '/n8n-chat': {
-        target: 'https://n8n-m287.onrender.com',
+      "/n8n-chat": {
+        target: "https://n8n-m287.onrender.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/n8n-chat/, ''),
+        rewrite: (path: string) => path.replace(/^\/n8n-chat/, ""),
       },
     },
   },
-})
+});
